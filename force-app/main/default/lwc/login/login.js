56da47268c467fc8f934ce9c6678900359496619
hhyyeeoonn <hyeon7102@gmail.com>
2024-01-15 16:54:53 +0900

    아이디, 패스워드, 확인 버튼 css 추가 완료

--- force-app/main/default/lwc/login/login.js
+++ force-app/main/default/lwc/login/login.js
@@ -1,5 +1,13 @@
 import { LightningElement, api } from 'lwc';
+import { loadStyle } from 'lightning/platformResourceLoader';
+import LOGIN_CSS from '@salesforce/resourceUrl/lwcLoginCss';
 
 export default class Login extends LightningElement {
     @api tabLogin;
+
+    
+    renderedCallback () {
+        Promise.all ([loadStyle (this, LOGIN_CSS)]);
+    } 
+    
 }
